b1c860b7e6cf082648b29ded467ff9dc696754bf
MagaPache <maga.pachem@gmail.com>
2018-06-16 03:55:41 -0300

    Consulta saldo adeudado por campaña funcional

--- src/Vista/ConsultaSaldoAdeudadoCampania.java
+++ src/Vista/ConsultaSaldoAdeudadoCampania.java
@@ -5,6 +5,20 @@
  */
 package Vista;
 
+import Controlador.GestorAgenteOficial;
+import Controlador.GestorCampania;
+import Controlador.GestorCliente;
+import Controlador.GestorPedido;
+import Modelo.AgenteOficial;
+import Modelo.Campania;
+import Modelo.VmPedidoCliente;
+import java.awt.event.ItemEvent;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.DefaultComboBoxModel;
+
 /**
  *
  * @author Usuario
@@ -14,8 +28,23 @@ public class ConsultaSaldoAdeudadoCampania extends javax.swing.JFrame {
     /**
      * Creates new form ConsultaSaldoAdeudadoCampania
      */
-    public ConsultaSaldoAdeudadoCampania() {
+    GestorAgenteOficial gao = new GestorAgenteOficial();
+    GestorCampania gc = new GestorCampania();
+    GestorPedido gp = new GestorPedido();
+    GestorCliente gcl = new GestorCliente();
+
+    public ConsultaSaldoAdeudadoCampania() throws SQLException {
         initComponents();
+        loadCmbOfficialAgent(gao.getOfficialAgents());
+        loadCmbCampaignsPerOfficialAgent(gc.getCampaignPerOfficialAgent(((AgenteOficial) cmbOfficialAgent.getSelectedItem()).getIdOfficialAgent()));
+        int idAgent = ((AgenteOficial) cmbOfficialAgent.getSelectedItem()).getIdOfficialAgent();
+        int idCampaign = ((Campania) cmbCampaign.getSelectedItem()).getIdCampaign();
+        loadCmbClient(gp.getClientOrderByCampaign(idCampaign, idAgent));
+        int idCliente = ((VmPedidoCliente) cmbClient.getSelectedItem()).getIdClient();
+        float montoAdeudado = gcl.getAmountOwedPerClientPerCampaign(idCliente, idAgent, idCampaign);
+        float montoAbonado = gcl.getAmountPayedPerClientPerCampaign(idCliente, idAgent, idCampaign);
+        float saldoCliente = montoAdeudado - montoAbonado;
+        lblAmountOwed.setText(Float.toString(saldoCliente));
     }
 
     /**
@@ -47,10 +76,25 @@ public class ConsultaSaldoAdeudadoCampania extends javax.swing.JFrame {
         jLabel3.setText("Cliente");
 
         cmbCampaign.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cmbCampaign.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                cmbCampaignItemStateChanged(evt);
+            }
+        });
 
         cmbOfficialAgent.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cmbOfficialAgent.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                cmbOfficialAgentItemStateChanged(evt);
+            }
+        });
 
         cmbClient.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cmbClient.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                cmbClientItemStateChanged(evt);
+            }
+        });
 
         jPanel1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
 
@@ -128,6 +172,61 @@ public class ConsultaSaldoAdeudadoCampania extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void cmbOfficialAgentItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_cmbOfficialAgentItemStateChanged
+        // TODO add your handling code here:
+        if (evt.getStateChange() == ItemEvent.SELECTED) {
+            try {
+                if (cmbOfficialAgent.getItemCount() > 0) {
+                    int idAgente = ((AgenteOficial) cmbOfficialAgent.getSelectedItem()).getIdOfficialAgent();
+                    loadCmbCampaignsPerOfficialAgent(gc.getCampaignPerOfficialAgent(idAgente));
+                    System.out.println(idAgente);
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(AltaProducto.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }//GEN-LAST:event_cmbOfficialAgentItemStateChanged
+
+    private void cmbCampaignItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_cmbCampaignItemStateChanged
+        // TODO add your handling code here:
+        int idAgent = ((AgenteOficial) cmbOfficialAgent.getSelectedItem()).getIdOfficialAgent();
+        if (evt.getStateChange() == ItemEvent.SELECTED) {
+            try {
+                if (cmbCampaign.getItemCount() > 0) {
+                    int idCampaign = ((Campania) cmbCampaign.getSelectedItem()).getIdCampaign();
+                    System.out.println(idAgent);
+                    System.out.println(idCampaign);
+                    loadCmbClient(gp.getClientOrderByCampaign(idCampaign, idAgent));
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(AltaProducto.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }//GEN-LAST:event_cmbCampaignItemStateChanged
+
+    private void cmbClientItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_cmbClientItemStateChanged
+        // TODO add your handling code here:
+        if (evt.getStateChange() == ItemEvent.SELECTED) {
+            if (cmbCampaign.getItemCount() > 0) {
+                try {
+                    //meter codigo
+                    int idAgente = ((AgenteOficial) cmbOfficialAgent.getSelectedItem()).getIdOfficialAgent();
+                    int idCampaign = ((Campania) cmbCampaign.getSelectedItem()).getIdCampaign();
+                    int idCliente = ((VmPedidoCliente) cmbClient.getSelectedItem()).getIdClient();
+                    float montoAdeudado = gcl.getAmountOwedPerClientPerCampaign(idCliente, idAgente, idCampaign);
+                    float montoAbonado = gcl.getAmountPayedPerClientPerCampaign(idCliente, idAgente, idCampaign);
+                    float saldoCliente = montoAdeudado - montoAbonado;
+                    lblAmountOwed.setText(Float.toString(saldoCliente));
+                } catch (SQLException ex) {
+                    Logger.getLogger(AltaCobro.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+        }
+    }//GEN-LAST:event_cmbClientItemStateChanged
+
     /**
      * @param args the command line arguments
      */
@@ -158,7 +257,11 @@ public class ConsultaSaldoAdeudadoCampania extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ConsultaSaldoAdeudadoCampania().setVisible(true);
+                try {
+                    new ConsultaSaldoAdeudadoCampania().setVisible(true);
+                } catch (SQLException ex) {
+                    Logger.getLogger(ConsultaSaldoAdeudadoCampania.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         });
     }
@@ -175,4 +278,28 @@ public class ConsultaSaldoAdeudadoCampania extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lblAmountOwed;
     // End of variables declaration//GEN-END:variables
+
+    private void loadCmbOfficialAgent(ArrayList<AgenteOficial> getOfficialAgents) {
+        DefaultComboBoxModel model = new DefaultComboBoxModel();
+        for (AgenteOficial officialAgent : getOfficialAgents) {
+            model.addElement(officialAgent);
+        }
+        cmbOfficialAgent.setModel(model);
+    }
+
+    private void loadCmbCampaignsPerOfficialAgent(ArrayList<Campania> campaigns) {
+        DefaultComboBoxModel model = new DefaultComboBoxModel();
+        for (Campania campaign : campaigns) {
+            model.addElement(campaign);
+        }
+        cmbCampaign.setModel(model);
+    }
+
+    private void loadCmbClient(ArrayList<VmPedidoCliente> clientsOrders) {
+        DefaultComboBoxModel model = new DefaultComboBoxModel();
+        for (VmPedidoCliente order : clientsOrders) {
+            model.addElement(order);
+        }
+        cmbClient.setModel(model);
+    }
 }
